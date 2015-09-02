ac5d506cc5f26c26f1e49e6135ead78ce75f2e76
sbezboro <sergei.b.ru@gmail.com>
2015-09-02 02:35:02 -0700

    python 3 compatibility for daemon

--- mcrunner/daemon.py
+++ mcrunner/daemon.py
@@ -25,6 +25,14 @@ import sys
 import time
 import signal
 
+# Python 3+ does not support unbuffered text I/O,
+# nor does it have the 'file' built-in function
+if sys.version_info[0] > 2:
+    buffering = 1
+    file = open
+else:
+    buffering = 0
+
 
 class Daemon(object):
     """
@@ -33,7 +41,8 @@ class Daemon(object):
     """
     def __init__(self, pidfile, stdin=os.devnull,
                  stdout=os.devnull, stderr=os.devnull,
-                 home_dir='.', umask=022, verbose=1, use_gevent=False):
+                 home_dir='.', umask=int('022', 8),
+                 verbose=1, use_gevent=False):
         self.stdin = stdin
         self.stdout = stdout
         self.stderr = stderr
@@ -55,7 +64,8 @@ class Daemon(object):
             if pid > 0:
                 # Exit first parent
                 sys.exit(0)
-        except OSError, e:
+        except OSError:
+            e = sys.exc_info()[1]
             sys.stderr.write(
                 "fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
             sys.exit(1)
@@ -71,7 +81,8 @@ class Daemon(object):
             if pid > 0:
                 # Exit from second parent
                 sys.exit(0)
-        except OSError, e:
+        except OSError:
+            e = sys.exc_info()[1]
             sys.stderr.write(
                 "fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
             sys.exit(1)
@@ -83,7 +94,7 @@ class Daemon(object):
             si = file(self.stdin, 'r')
             so = file(self.stdout, 'a+')
             if self.stderr:
-                se = file(self.stderr, 'a+', 0)
+                se = file(self.stderr, 'a+', buffering)
             else:
                 se = so
             os.dup2(si.fileno(), sys.stdin.fileno())
@@ -104,7 +115,7 @@ class Daemon(object):
             signal.signal(signal.SIGINT, sigtermhandler)
 
         if self.verbose >= 1:
-            print "Started"
+            print("Started")
 
         # Write pidfile
         atexit.register(
@@ -121,7 +132,7 @@ class Daemon(object):
         """
 
         if self.verbose >= 1:
-            print "Starting..."
+            print("Starting...")
 
         # Check for a pidfile to see if the daemon already runs
         try:
@@ -148,7 +159,7 @@ class Daemon(object):
         """
 
         if self.verbose >= 1:
-            print "Stopping..."
+            print("Stopping...")
 
         # Get the pid from the pidfile
         pid = self.get_pid()
@@ -173,17 +184,17 @@ class Daemon(object):
                 i = i + 1
                 if i % 10 == 0:
                     os.kill(pid, signal.SIGHUP)
-        except OSError, err:
-            err = str(err)
+        except OSError:
+            err = str(sys.exc_info()[1])
             if err.find("No such process") > 0:
                 if os.path.exists(self.pidfile):
                     os.remove(self.pidfile)
             else:
-                print str(err)
+                print(err)
                 sys.exit(1)
 
         if self.verbose >= 1:
-            print "Stopped"
+            print("Stopped")
 
     def restart(self):
         """
@@ -207,11 +218,11 @@ class Daemon(object):
         pid = self.get_pid()
 
         if pid is None:
-            print 'Process is stopped'
+            print('Process is stopped')
         elif os.path.exists('/proc/%d' % pid):
-            print 'Process (pid %d) is running...' % pid
+            print('Process (pid %d) is running...' % pid)
         else:
-            print 'Process (pid %d) is killed' % pid
+            print('Process (pid %d) is killed' % pid)
 
         return pid and os.path.exists('/proc/%d' % pid)
 
