591f1e622b1ced0ab74eea02ebad44c2921b0e51
sbezboro <sergei.b.ru@gmail.com>
2019-01-28 22:39:13 -0800

    fix setup.py pytest args to fix coveralls

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class PyTest(TestCommand):
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.pytest_args = ['--cov']
+        self.pytest_args = []
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
